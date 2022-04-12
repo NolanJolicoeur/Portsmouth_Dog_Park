f24f87178170403f6cab0b82a0a6c6539f4b66af
NolanJolicoeur <89327000+NolanJolicoeur@users.noreply.github.com>
2022-04-12 15:17:51 -0400

    Update Dog Park Dashboard.R

--- Dog Park Dashboard.R
+++ Dog Park Dashboard.R
@@ -55,30 +55,31 @@ ui <- shinyUI(fluidPage(
 
 #Sever code (makes the ui work)
 server <- shinyServer(function(input, output, session) {
+  #How do I make a sepparate data frame based off this??
   data1 <- reactive({ 
     req(input$file1) 
     
     inFile1 <- input$file1 
     
-    df1 <- read.delim(inFile1$datapath, header = input$header, sep = input$sep,
+    df5 <- read.delim(inFile1$datapath, header = input$header, sep = input$sep,
                       quote = input$quote)
-    df1 <- read.delim(file.choose())
-    df1 <- as.data.frame(df1)
-    df1 <- df1[grep('00000',df1$D),]
-    df1 <- as.data.frame(df1)
-    df1 <- df1 %>% separate(df1, c('Year','Month','Day', 'Time','Blank', 'TrafX_Count','Blank2')) 
-    df1$Day <- as.numeric(df1$Day)
-    df1$TrafX_Count <- as.numeric(df1$TrafX_Count)
-    df1 <- sqldf("select Year, Day, Month, Time, sum(TrafX_Count) from df1 group by Day")
-    df1 <- rename(df1, 'TrafX_Count' = 'sum(TrafX_Count)')
-    df1$Human_Count <- round((0.3673 * df1$TrafX_Count),0)
-    df1$Dog_Count <- round((0.4591 * df1$TrafX_Count),0)
-    df1$Car_Count <- round((0.2959 * df1$TrafX_Count),0)
+    df5 <- read.delim(file.choose())
+    df5 <- as.data.frame(df5)
+    df5 <- df1[grep('00000',df5$D),]
+    df5 <- as.data.frame(df5)
+    df5 <- df5 %>% separate(df5, c('Year','Month','Day', 'Time','Blank', 'TrafX_Count','Blank2')) 
+    df5$Day <- as.numeric(df5$Day)
+    df5$TrafX_Count <- as.numeric(df5$TrafX_Count)
+    df5 <- sqldf("select Year, Day, Month, Time, sum(TrafX_Count) from df5")
+    df5 <- rename(df5, 'TrafX_Count' = 'sum(TrafX_Count)')
+    df5$Human_Count <- round((0.3673 * df5$TrafX_Count),0)
+    df5$Dog_Count <- round((0.4591 * df5$TrafX_Count),0)
+    df5$Car_Count <- round((0.2959 * df5$TrafX_Count),0)
   #this code block takes the input data file and converts it into a dataframe for daily breakdown.
 })
   
   output$myplot1 <- renderPlot({
-    ggplot(dfavghourly, aes(x = Time, group = 1))+
+    ggplot(df5, aes(x = Time, group = 1))+
       labs(title = 'Average Dog, Human, and Car Count by the Hour', x = 'Time', y = 'Count', color = 'Key:')+
       geom_line(aes(y = Dog_Count), color = 'green')+
       geom_point(aes(y = Dog_Count, color = 'Dogs'))+
@@ -91,4 +92,3 @@ server <- shinyServer(function(input, output, session) {
 })
 
 shinyApp(ui, server)
-
