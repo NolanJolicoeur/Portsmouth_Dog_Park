bd8642314aa8f72e1a46be840075bffa103b5a60
NolanJolicoeur <89327000+NolanJolicoeur@users.noreply.github.com>
2022-04-12 22:26:11 -0400

    Add files via upload

---
+++ Merge and Read Files.R
@@ -0,0 +1,23 @@
+library(sqldf)
+library(tidyr)
+files = grep(".TXT", dir('Shuttle_Files'), value = T)
+df <- data.frame(Year = character(),
+                 Month = character(),
+                 Day = character(), 
+                 Time = character(), 
+                 TrafX_Count = integer())
+stopifnot(class(df) == 'data.frame')
+stopifnot(length(colnames(df)) == 5)
+for (t in dir('Shuttle_Files')){
+  otherdf <- as.data.frame(t)
+  otherdf <- otherdf[grep('00000',otherdf$D),]
+  otherdf <- as.data.frame(otherdf)
+  otherdf <- otherdf %>% separate(otherdf, c('Year','Month','Day', 'Time','Blank', 'TrafX_Count','Blank2')) 
+  otherdf$TrafX_Count <- as.numeric(otherdf$TrafX_Count)
+  otherdf <- sqldf("select Year, Month, Day, Time, TrafX_Count from otherdf")
+  df = sqldf("
+               SELECT * FROM df
+               UNION
+               SELECT * FROM otherdf
+               ")
+} 
