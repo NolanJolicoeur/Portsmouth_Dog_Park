dc64183d36c45cbebe8c7f0bff60b72635885dda
nolanjolicoeur <nolan.jolicoeur@salve.edu>
2022-04-20 14:53:04 -0400

    Added markdown file

--- Manual Count Data Ratios.R
+++ Manual Count Data Ratios.R
@@ -1,4 +1,9 @@
 df <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQeQ0YiEBVASJN0EukDk3Y6CB8rf9aaQJ-d0ViW_nWYf_3gsHHcYu87eKQIgzFOpM_mV9stuA75x0zw/pub?gid=0&single=true&output=csv")
+df$Day_Name <- weekdays(df$Date, abbreviate = FALSE)
+weekdays1 <- c('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday')
+df$Day_Type <- factor((weekdays(df$Date, abbreviate = FALSE) %in% weekdays1), 
+                    levels=c(FALSE, TRUE), labels=c('Weekend', 'Weekday'))
+#This code chunk is giving the general ratio of people, dogs, and cars to Traf_X count 
 people_to_count_ratio <- (sum(df$People))/(sum(df$Actual_Total))
 dogs_to_count_ratio <- (sum(df$Dogs))/(sum(df$Actual_Total))
 cars_to_count_ratio <- (sum(df$Cars))/(sum(df$Actual_Total))
