d4a75cb107b69b71892c738b05b00c641a0c6399
nolanjolicoeur <nolan.jolicoeur@salve.edu>
2022-04-28 13:04:45 -0400

    Edited files

--- Merge and Read Files.R
+++ Merge and Read Files.R
@@ -1,27 +1,42 @@
-library(sqldf)
+library(glue)
+library(dplyr)
 library(tidyr)
-df <- data.frame(Year = character(),
-                 Month = character(),
-                 Day = character(), 
-                 Time = character(), 
-                 TrafX_Count = integer())
-stopifnot(class(df) == 'data.frame')
-stopifnot(length(colnames(df)) == 5)
+library(stringr)
+library(sqldf)
+
+process <- function(data1){
+  df <- as.data.frame(data1)
+  df <- df[grep('00000',df$D),]
+  df <- as.data.frame(df)
+  df <- df %>% separate(df, c('Year','Month','Day', 'Time','Blank', 'TrafX_Count','Blank2')) 
+  df <- sqldf("select Year, Month, Day, Time, TrafX_Count from df")
+  df$TrafX_Count <- as.numeric(df$TrafX_Count)
+  df$Date_Time <- paste( df$Year, df$Month, df$Day, df$Time)
+  df <- sqldf("select Year, Month, Day, Time, sum(TrafX_Count), Date_Time from df group by Date_Time")
+  df <- rename(df, 'TrafX_Count' = 'sum(TrafX_Count)')
+  return(df)
+}
+
+
+df1 <- data.frame(Year = character(),
+                  Month = character(),
+                  Day = character(), 
+                  Time = character(), 
+                  TrafX_Count = integer(),
+                  Date_Time = numeric())
+stopifnot(class(df1) == 'data.frame')
+stopifnot(length(colnames(df1)) == 6)
 files <- dir('Shuttle_Files')
 setwd('Shuttle_Files')
 for (t in files){
   otherdf <- read.delim(t)
-  otherdf <- as.data.frame(otherdf)
-  otherdf <- otherdf[grep('00000',otherdf$D),]
-  otherdf <- as.data.frame(otherdf)
-  otherdf <- otherdf %>% separate(otherdf, c('Year','Month','Day', 'Time','Blank', 'TrafX_Count','Blank2')) 
-  otherdf$TrafX_Count <- as.numeric(otherdf$TrafX_Count)
-  otherdf <- sqldf("select Year, Month, Day, Time, TrafX_Count from otherdf")
-  df = sqldf("
-               SELECT * FROM df
+  otherdf <- process(otherdf)
+  df1 = sqldf("
+               SELECT * FROM df1
                UNION
                SELECT * FROM otherdf
                ")
 } 
 setwd('../')
-saveRDS(df, 'File_Log.R')
+saveRDS(df1, 'File_Log.R')
+
