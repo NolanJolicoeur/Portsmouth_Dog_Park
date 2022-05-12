80f780266ca59dba238dbbd82764534d4d4a8e5e
nolanjolicoeur <nolan.jolicoeur@salve.edu>
2022-05-12 14:21:27 -0400

    new folder

--- app.R
+++ app.R
@@ -11,7 +11,7 @@ library(knitr)
 library(kableExtra)
 library(gridExtra)
 library(forcats)
-
+library(RSQLite)
 
 ui <- shinyUI(fluidPage(
   #Main Page Title and tab to upload files for dog park data and weather data. 
@@ -58,29 +58,69 @@ ui <- shinyUI(fluidPage(
   )))
 
 server <- shinyServer(function(input, output) {
-  log <- readRDS('~/Dog_Park/File_Log.R')
-  output$FileNames <- renderText(log)
+  df3 <- data.frame(Year = character(),
+                    Month = character(),
+                    Day = character(), 
+                    Time = character(), 
+                    TrafX_Count = integer(),
+                    Date_Time = numeric())
+  stopifnot(class(df3) == 'data.frame')
+  stopifnot(length(colnames(df3)) == 6)
+  files <- dir('Shuttle_Files')
+  setwd('Shuttle_Files')
+  for (t in files){
+    otherdf <- read.delim(t)
+    otherdf <- process(otherdf)
+    df3 = sqldf("
+               SELECT * FROM df3
+               UNION
+               SELECT * FROM otherdf
+               ")
+  } 
+  setwd('../')
+  
+  outputDir <- "~/Desktop/DPData"
+  
+  saveData <- function(data) {
+    data <- t(data)
+    # Create a unique file name
+    fileName <- sprintf("%s_%s.csv", as.integer(Sys.time()), digest::digest(data))
+    # Write the file to the local system
+    write.csv(
+      x = data,
+      file = file.path(outputDir, fileName), 
+      row.names = FALSE, quote = TRUE
+    )
+  }
+  
+  loadData <- function() {
+    # Read all the files into a list
+    files <- list.files(outputDir, full.names = TRUE)
+    data <- lapply(files, read.csv, stringsAsFactors = FALSE) 
+    # Concatenate all data together into one data.frame
+    data <- do.call(rbind, data)
+    data
+  }
+  
+  process <- function(data1){
+    df <- as.data.frame(data1)
+    df <- df[grep('00000',df$D),]
+    df <- as.data.frame(df)
+    df <- df %>% separate(df, c('Year','Month','Day', 'Time','Blank', 'TrafX_Count','Blank2')) 
+    df <- sqldf("select Year, Month, Day, Time, TrafX_Count from df")
+    df$TrafX_Count <- as.numeric(df$TrafX_Count)
+    df$Date_Time <- paste( df$Year, df$Month, df$Day, df$Time)
+    df <- sqldf("select Year, Month, Day, Time, sum(TrafX_Count), Date_Time from df group by Date_Time")
+    df <- rename(df, 'TrafX_Count' = 'sum(TrafX_Count)')
+    return(df)}
+  
   observeEvent(input$Click1,{
     file <- file.choose()
     NewFile <- read.delim(file)
-    process <- function(data1){
-      df <- as.data.frame(data1)
-      df <- df[grep('00000',df$D),]
-      df <- as.data.frame(df)
-      df <- df %>% separate(df, c('Year','Month','Day', 'Time','Blank', 'TrafX_Count','Blank2')) 
-      df <- sqldf("select Year, Month, Day, Time, TrafX_Count from df")
-      df$TrafX_Count <- as.numeric(df$TrafX_Count)
-      df$Date_Time <- paste( df$Year, df$Month, df$Day, df$Time)
-      df <- sqldf("select Year, Month, Day, Time, sum(TrafX_Count), Date_Time from df group by Date_Time")
-      df <- rename(df, 'TrafX_Count' = 'sum(TrafX_Count)')
-      return(df)}
     NewFile <- process(NewFile)
-    df3 <- readRDS('~/Dog_Park/Data_Log.R')
-    NewFile = sqldf("SELECT * FROM NewFile UNION SELECT * FROM df3")
-    saveRDS(NewFile, 'Data_Log.R')
-    saveRDS(file, 'File_Log.R')
-    output$FileNames <- renderText(file) 
+    saveData(NewFile)
   })
+  
   observeEvent(input$Click, {
     year1 <- input$Text1
     month1 <- input$Text2
@@ -107,30 +147,8 @@ server <- shinyServer(function(input, output) {
     
     
     #Create and view data frame for shuttle files
-    
-    process <- function(data1){
-      df <- as.data.frame(data1)
-      df <- df[grep('00000',df$D),]
-      df <- as.data.frame(df)
-      df <- df %>% separate(df, c('Year','Month','Day', 'Time','Blank', 'TrafX_Count','Blank2')) 
-      df <- sqldf("select Year, Month, Day, Time, TrafX_Count from df")
-      df$TrafX_Count <- as.numeric(df$TrafX_Count)
-      df$Date_Time <- paste( df$Year, df$Month, df$Day, df$Time)
-      df <- sqldf("select Year, Month, Day, Time, sum(TrafX_Count), Date_Time from df group by Date_Time")
-      df <- rename(df, 'TrafX_Count' = 'sum(TrafX_Count)')
-      return(df)
-    }
-    
-    
-    df3 <- data.frame(Year = character(),
-                      Month = character(),
-                      Day = character(), 
-                      Time = character(), 
-                      TrafX_Count = integer(),
-                      Date_Time = numeric())
-    stopifnot(class(df3) == 'data.frame')
-    stopifnot(length(colnames(df3)) == 6)
-    df3 <- readRDS('~/Dog_Park/Data_Log.R')
+    df3 <- loadData()
+    colnames(df3) <- c('Year','Month','Day','Time','TrafX_Count', 'Blank')
     df3 <- sqldf("select Year, Month, Day, Time, TrafX_Count from df3")
     df3$Human_Count <- round(((sum(df1$People)/sum(df1$TrafX_Count)) * df3$TrafX_Count),0)
     df3$Dog_Count <- round(((sum(df1$Dogs)/sum(df1$TrafX_Count)) * df3$TrafX_Count),0)
